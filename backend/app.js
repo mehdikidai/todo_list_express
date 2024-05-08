bab2f09c1d882dd75c63ec97dc0303400211e551
mehdi_kidai <mehdikidai@gmail.com>
2024-05-08 18:41:52 +0100

    add mysql

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
-
-
+import GetData from "./db/index.js";
 
 const app = express();
 
@@ -13,48 +11,39 @@ const port = 3000;
 
 app.use(cors());
 
-const data = [];
 
-app.get("/test", async (req, res) => {
-   
+
+app.get("/",async (req, res) => {
+
+    const rows = await GetData.getTodo()
+    res.json(rows);
+
 });
 
-app.get("/", (req, res) => {
-    res.json(data);
+app.post("/todo",async (req, res) => {
+
+    const rows = await GetData.addTodo(req.body.content)
+    res.status(201).json(rows);
+
 });
 
-app.post("/todo", (req, res) => {
-    data.unshift({
-        id: uuidv4(),
-        content: req.body.content,
-        done: false,
-    });
-    res.status(201).json(data);
+app.put("/todo",async (req, res) => {
+
+    const rows = await GetData.done(req.body.id)
+    res.status(201).json(rows);
+    
 });
 
-app.put("/todo", (req, res) => {
-    const findTodoId = data.findIndex((t) => t.id == req.body.id);
+app.delete("/todo/:id",async (req, res) => {
 
-    if (findTodoId == -1) {
-        res.status(401);
-        return;
-    }
+    const rows = await GetData.deleteTodo(req.params.id)
+    res.status(201).json(rows);
 
-    data[findTodoId].done = !data[findTodoId].done;
-
-    res.status(201).json({ s: "ok" });
 });
 
-app.delete("/todo/:id", (req, res) => {
-    const findTodoId = data.findIndex((t) => t.id == req.params.id);
-    if (findTodoId == -1) {
-        res.status(401);
-        return;
-    }
-    data.splice(findTodoId, 1);
-    res.status(201).json({ s: "ok" });
-});
+
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`app runnig - port ${port}`);
 });
--- backend/db/index.js
+++ backend/db/index.js
@@ -1,1 +1,57 @@
+import mysql from "mysql2";
 
+export const pool = mysql
+    .createPool({
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "todo",
+    })
+    .promise();
+
+class GetData {
+
+
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+
+    async getTodo() {
+        const [rows] = await pool.query("SELECT * FROM todo");
+        return rows;
+    }
+
+    async addTodo(content) {
+        const [result] = await pool.query(
+            `INSERT INTO todo (content) VALUES (?)`,
+            [content]
+        );
+        return {
+            id:result.insertId
+        };
+    }
+
+    async deleteTodo(id) {
+        const [result] = await pool.query(
+            `DELETE FROM todo WHERE todo.id = ?`,
+            [id]
+        );
+        return {
+            affectedRows:result.affectedRows
+        };
+    }
+
+    async done(id){
+
+        const [result] = await pool.query(`UPDATE todo SET done = 1 ^ done WHERE todo.id = ?`,[id])
+
+        return {
+            changedRows:result.changedRows
+        };
+
+    }
+
+}
+
+export default new GetData(pool);
